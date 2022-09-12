122f1bfacec37f24f70a479bc87db79e9860e661
drgj <dydwns7936@naver.com>
2022-09-12 22:48:38 +0900

    JavaEdu_220912

---
+++ src/Pet.java
@@ -0,0 +1,4 @@
+public interface Pet {
+    void bark();
+
+}
---
+++ src/RobotDog.java
@@ -0,0 +1,28 @@
+public class RobotDog extends Robot implements Pet{
+
+    @Override
+    void move() {
+        System.out.println("child move");
+    }
+
+    @Override
+    void charging() {
+        System.out.println(getName() + " : charging");
+    }
+
+    @Override
+    public void bark() {
+        System.out.println("woof woof");
+    }
+
+    public static void main(String[] args) {
+        RobotDog rd = new RobotDog();
+
+        rd.setName("dog");
+        rd.bark();
+        rd.move();
+        rd.charging();
+
+
+    }
+}
